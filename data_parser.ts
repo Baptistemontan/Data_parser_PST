7d3f33d429747629abb919ec1b6929cb49eb4219
Bamontan <baptiste.demontangon@gmail.com>
2021-03-26 23:30:08 +0100

    comment + reverse sorting, [0] is now most recent

--- data_parser.ts
+++ data_parser.ts
@@ -1,16 +1,25 @@
 import * as fs from 'fs';
 import {Driver, DriverEvent} from './type'
 
-
+// array of driver, to be filled with the txt file
 const Data:Driver[] = [];
 
 fs.readFile("./Data_Batch.txt", {encoding:"utf8"},(err, data) => {
   console.time("temps");
+
+  // error handling
   if(err) {
+    // "handling" just print the error lol
     return console.log(err);
   }
-   data.split('\n').forEach(line => {
+  // file is a bunch of event line
+  // each line is :
+  // eventTime|truckId|driverId|driverName|routeId|routeName|latitude|longitude|speed|eventType|foggy|rainy|windy|congestionLevel
+  // first separate each line
+  data.split('\n').forEach(line => {
+    // then separate all values of the line
     const parsedLine = line.split('|');
+    // construct the driver
     const currentDriver:Driver = {
       truckId: parseInt(parsedLine[1]),
       driverId: parseInt(parsedLine[2]),
@@ -18,6 +27,7 @@ fs.readFile("./Data_Batch.txt", {encoding:"utf8"},(err, data) => {
       events: [],
       active: true
     }
+    // construct the event
     const event:DriverEvent={
       eventTime: new Date(parseInt(parsedLine[0])),
       routeId: parseInt(parsedLine[4]),
@@ -33,18 +43,28 @@ fs.readFile("./Data_Batch.txt", {encoding:"utf8"},(err, data) => {
       windy: parsedLine[12] == "1",
       congestionLevel: parseInt(parsedLine[13])
     }
-    if(!Data.find(driver => driver.driverName === currentDriver.driverName)?.events.push(event)) {
+    // if their already exist a driver with the same name
+    // push the event to its event array
+    if(!Data.find(driver => driver.driverId === currentDriver.driverId)?.events.push(event)) {
+      // else push the event to the current driver event array
       currentDriver.events.push(event);
+      // and push the driver to main array
       Data.push(currentDriver);
     }
   })
-  Data.forEach(driver =>  driver.events.sort((a,b) => a.eventTime.getTime() - b.eventTime.getTime()))
+  // for each driver sort the event in inverse chronology
+  // driver.event[0] must be most recent
+  Data.forEach(driver =>  driver.events.sort((a,b) => b.eventTime.getTime() - a.eventTime.getTime()))
+  // then write main array to a json file
   fs.writeFile("./data.json", JSON.stringify(Data), err => {
     if(err) {
+      // lol best err handling
       console.log(err)
     } else {
       console.log("Parsing successfull")
     }
+
+    // output time elapsed for data parsing
     console.timeEnd("temps");
   })
 })
